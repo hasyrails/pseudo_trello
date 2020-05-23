238afc7ff1a45efaefb8c15e8263d066fce37bab
Shota <runteq.learning.hasy@gmail.com>
2020-05-23 17:43:00 +0900

    [feat] セッション機能（ログイン・ログアウト機能）の実装
    セッション機能（ログイン・ログアウト機能）実装
    ヘッダーのログアウトリンク設定完了
    ログイン・ログアウトのリダイレクト先をトップページ(root_path)へ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'top#index'
   resources :users
+  resources :user_sessions
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  get 'logout', to: 'user_sessions#destroy'
+  post 'logout', to: 'user_sessions#destroy'
 end
