14999b901688ffd2306dd59e65d3b47be8766527
Shota <runteq.learning.hasy@gmail.com>
2020-05-24 13:46:33 +0900

    [feat]カード編集機能の実装
    controller) CardController#edit/updateの定義
    view) カード編集画面の作成
    view) 詳細画面に編集画面へのリンクを追加
    style) 編集画面のスタイルを設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -19,6 +19,19 @@ class CardsController < ApplicationController
     @card = Card.find_by(id: params[:id])
   end
 
+  def edit
+    @card = Card.find_by(id: params[:id])
+  end
+
+  def update
+    @card = Card.find_by(id: params[:id])
+    if @card.update_attributes(card_params)
+      redirect_to :root
+    else
+      render action: :edit
+    end
+  end
+
   private
 
   def card_params
